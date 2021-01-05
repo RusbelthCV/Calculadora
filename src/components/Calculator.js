cd85e30a14457e37c96205dd7c2e693806ed4a63
RusbelthCV <49898288+RusbelthCV@users.noreply.github.com>
2021-01-05 21:04:22 +0100

    Calculadora finalizada

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,77 @@
 import React, { Component } from 'react';
 import "../assets/css/calculadora.css";
-import Functions from './Functions';
 
 class Calculator extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            pantalla: "0",
+            resultado: 0
+        };
         this.ponerNumeros = this.ponerNumeros.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.calculate = this.calculate.bind(this);
+        this.clearAll = this.clearAll.bind(this);
+    }
+    clearAll = () => {
+        this.setState({
+            pantalla: "0",
+            resultado: 0
+        });
+    }
+    calculate = () =>{
+        let pantalla = this.state.pantalla;
+        let resultado = eval(pantalla);
+        this.setState({
+            pantalla: resultado,
+            resultado: 0
+        });
+    }
+    handleClick = (e) =>{
+        let valorBoton = e.target.innerText;
+        let pantalla = document.getElementsByClassName("textDiv")[0].textContent;
+        if(pantalla === "0"){
+            pantalla = ""
+            pantalla += valorBoton;
+        }
+        else{
+            pantalla += valorBoton;
+        }
+        this.setState({pantalla: pantalla})
+        if(e.target.dataset.action){
+            if(e.target.dataset.action === "clear"){
+                this.clearAll();
+            }
+        }
+        else{
+            
+        }
     }
     ponerNumeros = () =>{
         let numeros = [7,8,9,4,5,6,1,2,3,0];
         return (numeros.map((element, key)=>{
             return(
-            <button key={key}>
+            <button key={key} onClick = {this.handleClick}>
                 {element}
             </button>)
         }))
     }
     render() {
-        <Functions add = {this.add}/>
         return (
         <div className = "container">
-            <div className="textDiv">0</div>
-            <button className="key--operator" data-action="add">+</button>
-            <button className="key--operator" data-action="subtract">-</button>
-            <button className="key--operator" data-action="multiply">&times;</button>
-            <button className="key--operator" data-action="divide">÷</button>
+            <div className="textDiv">{this.state.pantalla}</div>
+            <button className="key--operator" data-action="add" onClick={this.handleClick}>+</button>
+            <button className="key--operator" data-action="subtract" onClick={this.handleClick}>-</button>
+            <button className="key--operator" data-action="multiply" onClick={this.handleClick}>*</button>
+            <button className="key--operator" data-action="divide" onClick={this.handleClick}>/</button>
             {this.ponerNumeros()}
-            <button data-action="decimal">.</button>
-            <button data-action="clear">AC</button>
-            <button className="key--equal" data-action="calculate" onClick={Functions.add()}>=</button>
+            <button data-action="decimal" onClick={this.handleClick}>.</button>
+            <button data-action="clear" onClick={this.handleClick}>AC</button>
+            <button className="key--equal" data-action="calculate" onClick={this.calculate}>=</button>
         </div>
+        
         )
+        
     }
 }
 
